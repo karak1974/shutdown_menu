ee4d2019e8dcb9812def91239583e2d516f5e39f
WoLfY <58589088+karak1974@users.noreply.github.com>
2021-03-01 23:40:34 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+main () {
+sudo cp shutdown_module.py /usr/bin/shutdown_module
+sudo chmod +x /usr/bin/brightness
+
+read -p "Do you want to add it to i3 config?[Y/N]: " menu
+    if [[ $menu == "Y"  || $menu == "Yes" || $menu == "y" || $menu == "yes" ]]
+    then
+        ID=$(whoami)
+        echo " " >> /home/${ID}/.config/i3/config
+        echo " " >> /home/${ID}/.config/i3/config
+        echo "bindsym Mod1+Shift+s exec shutdown_module inFloatingMode" >> /home/${ID}/.config/i3/config
+        echo "for_window [class="Shutdown_module"] floating enable" >> /home/${ID}/.config/i3/config
+    elif [[ $menu == "N"  || $menu == "No" || $menu == "n" || $menu == "no" ]]
+    then
+        echo "There is nothing to do"
+    else
+        echo "Invalid option"
+    fi
+}
+
+if [ "$EUID" -ne 0 ]
+then
+    main
+else
+    echo "Please don't run as root"
+    exit
+fi
