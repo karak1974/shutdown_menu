11d7146c556252642ec70fcc88627e1fc1acf4a2
WoLfY <58589088+karak1974@users.noreply.github.com>
2021-03-02 08:46:18 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ main () {
 sudo cp shutdown_module.py /usr/bin/shutdown_module
 sudo chmod +x /usr/bin/shutdown_module
 pip install PyGObject
+menu="Y"
 
 read -p "Do you want to add it to i3 config?[Y/N]: " menu
-    if [[ $menu == "Y"  || $menu == "Yes" || $menu == "y" || $menu == "yes" ]]
+    if [[ $menu == "Y" || $menu == "y" ]]
     then
         ID=$(whoami)
         echo " " >> /home/${ID}/.config/i3/config
-        echo " " >> /home/${ID}/.config/i3/config
         echo 'bindsym $mod+Shift+s exec shutdown_module inFloatingMode' >> /home/${ID}/.config/i3/config
         echo 'for_window [class="Shutdown_module"] floating enable' >> /home/${ID}/.config/i3/config
-    elif [[ $menu == "N"  || $menu == "No" || $menu == "n" || $menu == "no" ]]
+    elif [[ $menu == "N" || $menu == "n" ]]
     then
         echo "There is nothing to do"
     else
